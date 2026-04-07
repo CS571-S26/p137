282556dc66c4d9bd685ad7cca9b8aa90956d6c05
Krish Chauhan <toughkrish@gmail.com>
2026-04-07 10:21:02 -0500

    fix config to /p137/ instead of /p0/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/p0/',
+  base: '/p137/',
   plugins: [react()],
   build: {
     outDir: "docs"
